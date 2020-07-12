cd9c4c11fd289f1227c2dd6dfc743f5ea8392962
varshini-github <vadladivarshini@gmil.com>
2020-07-11 20:03:46 -0700

    Added TextView object

--- app/src/main/java/com/example/location/MainActivity.java
+++ app/src/main/java/com/example/location/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.location;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
